253f5271b2aadc2a9e9a6d7f919eb7cdbc33c2df
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2017-07-24 13:01:46 +0530

    further development to project

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
-from .models import Album
-from django.shortcuts import render
+from .models import Album, Song
+from django.shortcuts import render,get_object_or_404
 
 
 def index(request):
@@ -8,8 +7,16 @@ def index(request):
     return render(request,"music/index.html",{'all_albums':all_albums})
 
 def detail(request,album_id):
-    try:
-        album = Album.objects.get(pk = album_id)
-    except Album.DoesNotExist:
-        raise Http404("Resource not available")
+    album = get_object_or_404(Album,pk=album_id)
     return render(request,"music/detail.html",{'album':album})
+
+def favorite(request,album_id):
+    album = get_object_or_404(Album,pk = album_id)
+    try:
+        selected_song = album.song_set.get(pk = request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request,"music/detail.html",{'album':album, 'error_message': "You selected a invalid song"})
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request,"music/detail.html",{'album':album})
